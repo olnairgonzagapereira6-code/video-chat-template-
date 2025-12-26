e223645dd69bda9152462d5a83508f71c62d5a0c
olnairgonzagapereira6-code <olnairgonzagapereira6@gmail.com>
2025-12-26 10:36:23 -0300

    Delete src directory

--- src/test/setup.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
-
-describe('setup', () => {
-  it('should provide jest-dom matchers', () => {
-    const { getByText } = render(<div>Hello World</div>);
-    expect(getByText('Hello World')).toBeInTheDocument();
-  });
-});
